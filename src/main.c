b56c44ff396b600c48e1c10b3d5e34ee8d58c4ab
nanda-gopal-sb <nandagopal.nsb@gmail.com>
2025-07-09 13:11:32 +0530

    main game loop

--- src/main.c
+++ src/main.c
@@ -6,5 +6,18 @@
 
 int main()
 {
-    printf("HelloWorld");
+
+    const int screen_height = 800;
+    const int screen_width = 450;
+    InitWindow(screen_height, screen_width, "LMAO");
+    // srand(time(NULL));
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLUE);
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
